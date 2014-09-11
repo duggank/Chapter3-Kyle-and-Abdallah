3b473358d3c7c16e031cf50a66c5429755c4f858
duggank <CreativeWeep@gmail.com>
2014-09-11 08:39:02 -0400

    Final Commit
    Finished

---
+++ Chapter3/Payroll/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Payroll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            double hpr, hours, gross, withhold, net;
+
+            Console.Write("Hourly pay rate: ");
+            input = Console.ReadLine();
+            hpr = Convert.ToDouble(input);
+
+            Console.Write("hours worked: ");
+            input = Console.ReadLine();
+            hours = Convert.ToDouble(input);
+
+            gross = hpr * hours;
+            Console.WriteLine("Gross pay:                    {0}", gross.ToString("C"));
+
+            if (gross <= 300)
+            {
+                withhold = gross * .10;
+                Console.WriteLine("withholding tax:               {0}", withhold.ToString("C"));
+            }
+            else
+            {
+                withhold = gross * .12;
+                Console.WriteLine("withholding tax:               {0}", withhold.ToString("C"));
+            }
+
+            net = gross - withhold;
+            Console.WriteLine("Net:                          {0}", net.ToString("C"));
+        }
+    }
+}
